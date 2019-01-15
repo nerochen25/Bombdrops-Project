f84af04fe6ea5ff7efca9f6fdff1dbc51d0aa5b3
Nero <nerochen25@berkeley.edu>
2019-01-14 16:17:57 -0800

    Add score_board.js and start creating player class

--- src/player.js
+++ src/player.js
@@ -1,3 +1,12 @@
 // function playerNameInput() {
 //     document.getElementById('player_name')
 // }
+const playerName = document.getElementById('player_name').value;
+
+
+class Player {
+    constructor(options) {
+        this.name = options.name
+        this.score = options.score
+    }
+}
